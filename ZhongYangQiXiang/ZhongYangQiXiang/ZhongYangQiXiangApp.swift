c44dcc8292b79ee9ce6c8fedd0b71e6022d29889
wangjie0223 <565241473@qq.com>
2025-03-20 16:22:10 +0800

    SwiftUI 基础工程，创建标签栏

---
+++ ZhongYangQiXiang/ZhongYangQiXiang/ZhongYangQiXiangApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhongYangQiXiangApp.swift
+//  ZhongYangQiXiang
+//
+//  Created by 王杰 on 2025/3/20.
+//
+
+import SwiftUI
+
+@main
+struct ZhongYangQiXiangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
